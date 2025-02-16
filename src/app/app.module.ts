cf0ef2562bd38cf3651ec571b9a8ed6fa14488cc
Tomasz Słupik <slupiktomasz@gmail.com>
2025-02-16 08:36:43 +0100

    Dodanie formularza Reactive Forms Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'; 
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ReservationFormComponent } from './reservation-form/reservation-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ReservationFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./reservation-form.component.css']
 })
 export class ReservationFormComponent {
+  reservationForm: FormGroup
+
+  constructor(private fb: FormBuilder) {
+
+    this.reservationForm = this.fb.group({
+      checkInDate: ['', Validators.required],  
+      checkOutDate: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+
+  }
 
 }
