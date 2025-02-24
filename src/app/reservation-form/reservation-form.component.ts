2be81676194052dd3105ad40f97c6f004191164d
Tomasz Słupik <slupiktomasz@gmail.com>
2025-02-24 08:55:45 +0100

    Dodanie walidacji

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,19 +6,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './reservation-form.component.html',
   styleUrls: ['./reservation-form.component.css']
 })
-export class ReservationFormComponent {
+export class ReservationFormComponent implements OnInit {
   reservationForm: FormGroup
+
+  ngOnInit(): void {}
 
   constructor(private fb: FormBuilder) {
 
     this.reservationForm = this.fb.group({
       checkInDate: ['', Validators.required],  
-      checkOutDate: ['', Validators.required]
+      checkOutDate: ['', Validators.required],
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      room: ['', Validators.required],
     });
   }
 
   onSubmit() {
+    if (this.reservationForm.valid) {
+      console.log('Formularz poprawny', this.reservationForm.value);
+    } else {
+      this.markAllAsTouched();
+    }
+  }
 
+  private markAllAsTouched() {
+    Object.keys(this.reservationForm.controls).forEach(field => {
+      const control = this.reservationForm.get(field);
+      control?.markAsTouched({ onlySelf: true });
+    });
   }
 
 }
